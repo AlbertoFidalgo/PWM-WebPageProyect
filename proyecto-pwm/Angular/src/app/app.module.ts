5c754f0690643aee0afd554fba3aedb95e2a05c3
Nauzet Déniz Perdomo <nauuuni@gmail.com>
2022-04-18 09:39:48 +0100

    Part of main page, responsive design and header in Angular

--- proyecto-pwm/Angular/src/app/app.module.ts
+++ proyecto-pwm/Angular/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CarousellComponent } from './carousell/carousell.component';
+import { SideBoxComponent } from './side-box/side-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CarousellComponent,
+    SideBoxComponent
   ],
   imports: [
     BrowserModule,
