c89b3338a6ab2037c0e0dba47d94ff5277d04f24
Alex <90198690+AlexanderOMT@users.noreply.github.com>
2022-04-28 21:46:19 +0100

    testeo (con éxito) de validación de formularios en register

--- proyecto-pwm/Angular/src/app/components/register/register.component.ts
+++ proyecto-pwm/Angular/src/app/components/register/register.component.ts
@@ -1,23 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {User} from "../../models/user.model";
 import {UserService} from "../../services/user.service";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {first} from "rxjs";
+import {error} from "@angular/compiler/src/util";
 
 @Component({
   selector: 'register-root',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
 
   user: User = new User();
   dataSubmitted = false;
+  form: FormGroup;
 
 
-  constructor(private userService: UserService) {
+  constructor(
+    private userService: UserService,
+    private formBuilder: FormBuilder
+  ) {  }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      name: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    this.dataSubmitted = true;
+
+    if (this.form.invalid) { console.log("Formulario incorrecto"); return; }
+
+    this.userService.create(this.form.value)
+
   }
 
 
+  get formField(){ return this.form.controls; }
 
   createUser():void{
     this.userService.create(this.user).then(() => {
@@ -27,9 +48,11 @@ export class RegisterComponent {
 
   }
 
+
   prepareForNewUser(): void {
     this.dataSubmitted = false;
     this.user = new User();
   }
+
 
 }
